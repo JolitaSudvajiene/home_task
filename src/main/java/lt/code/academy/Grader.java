01f8f42fe3dba63cd634c3f41cf0b1c278b529ae
Jolita Sudvajiene <sauleviciute.jolita@gmail.com>
2023-01-29 19:52:05 +0200

    komentarai

--- src/main/java/lt/code/academy/Grader.java
+++ src/main/java/lt/code/academy/Grader.java
@@ -20,44 +20,54 @@ import java.util.stream.IntStream;
 public class Grader {
 
     public ExamsResults compareAnswers(Results studentResults, ExamData correctAnswers) {
+        //Inicializuoja studentu atsakymu recordus
         ExamsResults studentScores = new ExamsResults(new ArrayList<>());
-
-        //kiekvienam studento irasui- istraukiama studento info (23 eilute), tada istraukiami to studento atsakymai (24 eilute)
-        // 25 eilute - paima irasu kieki ir kiekviena studento atsakyma tikrina su teisingu atsakymu is correctAnswers, kiek sutampa, deda i score
+        //stream kiekvieno studento rezultata ima ir atsakymus lygina su teisingais atsakymais
         studentResults.results().stream()
                 .forEach(result -> {
+                    //pasiima studento atsakymus - i lista
                     List<StudentAnswer> studentAnswers = result.answers();
+                    //palygina atsakymus su teisingais ir suskaiciuoja teisingu ats kieki
                     int score = (int) IntStream.range(0, studentAnswers.size())
                             .filter(i -> studentAnswers.get(i).answer().equalsIgnoreCase(correctAnswers.answers().get(i).answer()))
                             .count();
+                    //iskviecia metoda kuris ivertina rezultata ir suskaiciuoja pazymi
                     int grade = gradeExam(studentAnswers, score);
-                            studentScores.results().add(new ExamResult(result.studentExam().id(), result.studentExam().test_title(),
-                                    result.studentExam().type(), new ExamResultStudent(result.student().id(), result.student().name(),
-                                    result.student().surname(), grade)));
+                    //kuria nauja irasa rezultatu ir prideda egzamino informacija bei studento info ir pazymy
+                    studentScores.results().add(new ExamResult(result.studentExam().id(), result.studentExam().test_title(),
+                            result.studentExam().type(), new ExamResultStudent(result.student().id(), result.student().name(),
+                            result.student().surname(), grade)));
                 });
         return studentScores;
     }
 
 
+    //su bufferedWriter iraso i faila, taip pat cia panadojama data failo pavadinime todel failo pavadinimas nesikartoja
     public void writeStudentScoresToJson(ExamsResults studentScores) {
         ObjectMapper objectMapper = new ObjectMapper();
+        //ijungiamas formatavimas failo kurimui - space'ai json faile
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-        // Create a date stamp to use in the file name
+        //panaudoja LocalDateTime, now() metodas uzfiksuoja dabartini laika
         LocalDateTime now = LocalDateTime.now();
+        //nustatomas datos formatas
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+        //suformatuojama data
         String timestamp = now.format(formatter);
 
-        // Set the file path and name
+        //Failo pavadinimas
         String filePath = "student_scores_" + timestamp + ".json";
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
-            objectMapper.writeValue(bw, studentScores);
+        //Exception hadling'as
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath))) {
+            objectMapper.writeValue(bufferedWriter, studentScores);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    //suskaiciuoja pazymy pagal formule
+    //integeri castinam i double ir tada atagl i integer ir apvalinam
     public int gradeExam(List<StudentAnswer> studentAnswers, int correctAnswers) {
         double examScore = (double) (correctAnswers * 10) / studentAnswers.size();
         return (int) Math.round(examScore);
--- src/main/java/lt/code/academy/ReadCorrectAnswers.java
+++ src/main/java/lt/code/academy/ReadCorrectAnswers.java
@@ -10,11 +10,13 @@ public class ReadCorrectAnswers {
 
     ExamData correctAnswers;
 
+    //nuskaito answers.json faila ir deserializuoja pagal ExamData recorda
     public ReadCorrectAnswers() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         correctAnswers = objectMapper.readValue(new File("answers.json"), ExamData.class);
     }
 
+    //grazinta uzpildytus recordus, ju lista
     public ExamData getCorrectAnswers() {
         return correctAnswers;
     }
